e69e8edcbb3c672fa9ea203a996f1d9d60198ee4
Venexia Walker <venexia.walker@bristol.ac.uk>
2020-05-19 14:23:30 +0100

    Add echo to source commands

--- Hypertension.R
+++ Hypertension.R
@@ -10,37 +10,37 @@ graphics.off()
 
 # Plot main analysis results ==================================================
 
-source("code/figure_main.R")
+source("code/figure_main.R", echo = TRUE)
 
 # Plot sensitivity analysis results ===========================================
 
-source("code/figure_sensitivity.R")
+source("code/figure_sensitivity.R", echo = TRUE)
 
 # Plot adjusted results =======================================================
 
-source("code/figure_adj.R")
+source("code/figure_adj.R", echo = TRUE)
 
 # Plot subtypes analysis results ==============================================
 
-source("code/figure_subtypes.R")
+source("code/figure_subtypes.R", echo = TRUE)
 
 # Plot logit results ==========================================================
 
-source("code/figure_logit.R")
+source("code/figure_logit.R", echo = TRUE)
 
 # Plot heat map figures (not included in paper) ===============================
 
-# source("code/figure_refclasses.R")
+# source("code/figure_refclasses.R", echo = TRUE)
 
 # Plot bias scatter ===========================================================
 
-source("code/figure_biascomponent.R")
+source("code/figure_biascomponent.R", echo = TRUE)
 
 # Plot instrument frequency ===================================================
 
-source("code/figure_insfreq.R")
+source("code/figure_insfreq.R", echo = TRUE)
 
 # Format table containing Bonet's instrumental variable inequality tests) =====
 
-source("code/table_bonet.R")
+source("code/table_bonet.R", echo = TRUE)
 
